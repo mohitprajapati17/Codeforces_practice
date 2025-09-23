e41ad79bdfa53c19e6a02fee713b7ceac7040091
mohitprajapati17 <mohitprajapati2444@gmail.com>
2025-09-23 23:50:47 +0530

    one more done

--- EC_182_div2_C.cpp
+++ EC_182_div2_C.cpp
@@ -6,65 +6,41 @@
 #include <math.h>
 using namespace std;
 #define ll long long
-ll MOD =1e9+7;
+ll MOD =998244353;
 
 #define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
  
 
 void solve() {
-   ll n;
+   int n;
    cin>>n;
    vector<int> arr(n);
-   set<int> st;
-   for(int i=0;i<n;i++){
-    cin>>arr[i];
-    if(arr[i]!=0) st.insert(arr[i]);
+   for(int i=0;i<n;i++) cin>>arr[i];
+   if(arr[n-1]==1&&arr[0]==2||arr[0]==1&&arr[n-1]==2){
+        int l=1;
+        int r=n-2;
+        while(l<r){
+            if(arr[l]!=arr[l-1]+2){
+                cout<<"No"<<endl;
+                return;
+            }
+            if(arr[r]!=arr[r+1]+2){
+                cout<<"No"<<endl;
+                return;
+            }
+            l++;
+            r--;
+        }
+
     
    }
-   vector<int> a;
-   for(int i=1;i<=n;i++){
-    if(st.find(i)!=st.end()){
-
-    }
-    else{
-        a.push_back(i);
-    }
-   }
-   sort(a.rbegin(),a.rend());
-   int i=0;
-   for(auto &  it: arr){
-    if(it==0){
-        it=a[i];
-        i++;
-    }
-   }
-   int cnt=0;
-   int l=-1;
-   int r=-1;
-   for(int i=0;i<n;i++){
-    if(arr[i]!=i+1){
-        l=i;
-        break;
-    }
-   }
-
-   for(int i=n-1;i>=0;i--){
-    if(arr[i]!=i+1){
-        r=i;
-        break;
-    }
-   }
-   if(l==-1&&r==-1) {
-    cout<<0<<endl;
+   else {
+    cout<<"No"<<endl;
     return;
    }
-   cout<<r-l+1<<endl;
-
-
-
-  
-
+   cout<<"Yes"<<endl;
+   
 
 
 }
